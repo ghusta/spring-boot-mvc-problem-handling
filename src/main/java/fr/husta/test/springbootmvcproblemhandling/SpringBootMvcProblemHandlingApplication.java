efb1eabed44ff6de5edc0326fb283ab343f9c699
Guillaume HUSTA <guillaume.husta@gmail.com>
2019-12-02 11:53:03 +0100

    Register ProblemModule for Jackson manually

--- src/main/java/fr/husta/test/springbootmvcproblemhandling/SpringBootMvcProblemHandlingApplication.java
+++ src/main/java/fr/husta/test/springbootmvcproblemhandling/SpringBootMvcProblemHandlingApplication.java
@@ -1,13 +1,22 @@
 package fr.husta.test.springbootmvcproblemhandling;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.zalando.problem.ProblemModule;
 
 @SpringBootApplication
 public class SpringBootMvcProblemHandlingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMvcProblemHandlingApplication.class, args);
-	}
+    @Bean
+    public ObjectMapper objectMapper() {
+		return new ObjectMapper()
+				.registerModule(new ProblemModule().withStackTraces(false));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvcProblemHandlingApplication.class, args);
+    }
 
 }
