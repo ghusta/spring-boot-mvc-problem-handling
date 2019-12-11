4ccbad24caa59025326c23d400e94c02efcb7457
Guillaume HUSTA <guillaume.husta@gmail.com>
2019-12-11 16:30:19 +0100

    Add javadoc

--- src/main/java/fr/husta/test/springbootmvcproblemhandling/error/TeapotException.java
+++ src/main/java/fr/husta/test/springbootmvcproblemhandling/error/TeapotException.java
@@ -3,6 +3,13 @@ package fr.husta.test.springbootmvcproblemhandling.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Will be handled by {@link fr.husta.test.springbootmvcproblemhandling.problem.advice.TeapotExceptionAdviceTrait TeapotExceptionAdviceTrait}
+ * (an {@link org.springframework.web.bind.annotation.ExceptionHandler ExceptionHandler}).
+ * <p>
+ * No need for {@link ResponseStatus @ResponseStatus}.
+ * </p>
+ */
 // @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
 public class TeapotException extends RuntimeException {
 
