5a620a1613f369496e5dba293c26977d8525d4af
Guillaume HUSTA <guillaume.husta@gmail.com>
2020-01-07 13:42:57 +0100

    Spring Boot : register ProblemModule (Jackson)

--- src/main/java/fr/husta/test/springbootmvcproblemhandling/SpringBootMvcProblemHandlingApplication.java
+++ src/main/java/fr/husta/test/springbootmvcproblemhandling/SpringBootMvcProblemHandlingApplication.java
@@ -1,13 +1,31 @@
 package fr.husta.test.springbootmvcproblemhandling;
 
+import com.fasterxml.jackson.databind.Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.zalando.problem.ProblemModule;
 
 @SpringBootApplication
 public class SpringBootMvcProblemHandlingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMvcProblemHandlingApplication.class, args);
-	}
+//    @Bean
+//    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+//        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+//        builder.modulesToInstall(new ProblemModule().withStackTraces(false));
+//        return builder;
+//    }
+
+    /**
+     * See : https://docs.spring.io/spring-boot/docs/current/reference/html/howto.html#howto-customize-the-jackson-objectmapper
+     */
+    @Bean
+    public Module problemModule() {
+        return new ProblemModule().withStackTraces(false);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvcProblemHandlingApplication.class, args);
+    }
 
 }
